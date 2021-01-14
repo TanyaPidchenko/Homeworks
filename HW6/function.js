75a12168786d93bd3efe756fe579425110a6c2c5
TanyaPidchenko <75585183+TanyaPidchenko@users.noreply.github.com>
2021-01-14 20:47:02 +0200

    Add files via upload

---
+++ HW6/function.js
@@ -0,0 +1,25 @@
+function drawTriangle(symbol, height) {
+    for (i = symbol; i.length <= height; i += symbol) {
+        if (height < 3) {
+            var height = 3;
+        }
+        console.log(i);
+    }
+}
+
+drawTriangle('%', 5);
+
+
+function toCapitalCase(line) {
+    var x = "";
+    for (i = 0; i < line.length; i++) {
+        if (line[i - 1] === " " || i === 0) {
+            x += line[i].toUpperCase();
+        } else {
+            x += line[i];
+        }
+    }
+    return x;
+}
+
+toCapitalCase("lorem ipsum stan" );
